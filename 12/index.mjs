e02d2a96a23dcd8445f449cdfabaaf19484ee9a6
Marco <99331256+MarkupMonkey@users.noreply.github.com>
2022-07-26 12:40:29 +0200

    await the lucky draw results

---
+++ 12/index.mjs
@@ -0,0 +1,26 @@
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+        const win = Boolean(Math.round(Math.random()));
+
+        process.nextTick(() => {
+            if (win) {
+                resolve(`${player} won a prize in the draw!`);
+            } else {
+                reject(new Error(`${player} lost the draw.`));
+            }
+        });
+    });
+}
+
+const getResults = async (...players) => {
+    try {
+        const results = await Promise.all(
+            players.map(async player => await luckyDraw(player))
+        );
+        console.log(results);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+getResults("Tina", "Jorge", "Julien");
