c043d7a5598dbfd8c57094ca40d9aa10d52abe5e
Marco <99331256+MarkupMonkey@users.noreply.github.com>
2022-07-26 12:18:49 +0200

    Used a method with a callback

--- 010/index.mjs
+++ 010/index.mjs
@@ -1,7 +1,6 @@
 import * as fs from "node:fs";
 
-fs.writeFile("file-for-you.txt", "What a wonderful day", "utf8", (err, data) => {
+fs.writeFile("file-for-you.txt", "What a wonderful day", "utf8", (err) => {
     if (err) throw err;
     console.log('Il messaggio è arrivato, file salvato\nThe message has arrived, file saved')
-    console.log(data);
 })
