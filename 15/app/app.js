e08950ce289c7d4bd1363afe1993d79380fcc532
Marco <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-02 16:00:21 +0200

    Exercise 19 Add support for CORS to your API

--- 15/app/app.js
+++ 15/app/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 const prisma = require('../lib/prisma/client');
 const {
     validate,
     validationErrorMiddleware,
 } = require('../lib/validation/index');
 const planetSchema = require('../lib/validation/planet');
+
+const corsOption = {
+    origin: 'http://localhost:8080'
+};
+
 app.use(express.json());
+app.use(cors(corsOption));
+
 app.get('/planets', async (req, res) => {
     const planets = await prisma.planet.findMany();
     res.json(planets);
