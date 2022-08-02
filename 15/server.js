1e8636a50c13f7fd3e9d90eeb89afa6e0bdf836f
Marco <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-02 12:52:02 +0200

    Exercise 17 - Create a resource

--- 15/server.js
+++ 15/server.js
@@ -5,11 +5,21 @@ const prisma = require('./lib/prisma/client');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/planets', async (req, res) => {
     const planets = await prisma.planet.findMany();
 
     res.json(planets);
 });
+
+app.post('/planets', async (req, res) => {
+    const planet = req.body;
+
+    res.status(201).json(planet);
+});
+
+
 
 app.listen(process.env.PORT, () => console.log(`server is running on port: ${process.env.PORT}`));
 
