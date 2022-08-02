9cd8f27ae5fb1c3e7fbe3350edb4c1673e0c3f02
Marco <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-02 12:08:04 +0200

    Exercise 16 Retrieve all resources

--- 15/server.js
+++ 15/server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 
+const prisma = require('./lib/prisma/client');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({ msg: 'Here comes the sun' });
+app.get('/planets', async (req, res) => {
+    const planets = await prisma.planet.findMany();
+
+    res.json(planets);
 });
 
 app.listen(process.env.PORT, () => console.log(`server is running on port: ${process.env.PORT}`));
