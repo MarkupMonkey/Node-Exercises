b8bf9f48c609510fb06068769458464176b9e1fb
Marco <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-02 15:39:39 +0200

    Exercise 18 Retrieve, update and delete a resource

--- 15/server.js
+++ 15/server.js
@@ -1,24 +1,5 @@
 require('dotenv').config();
-const express = require('express');
-
-const prisma = require('./lib/prisma/client');
-
-const app = express();
-
-app.use(express.json());
-
-app.get('/planets', async (req, res) => {
-    const planets = await prisma.planet.findMany();
-
-    res.json(planets);
-});
-
-app.post('/planets', async (req, res) => {
-    const planet = req.body;
-
-    res.status(201).json(planet);
-});
-
+const app = require('./app/app');
 
 
 app.listen(process.env.PORT, () => console.log(`server is running on port: ${process.env.PORT}`));
